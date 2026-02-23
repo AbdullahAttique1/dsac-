51053ee23f33022a6843ea2f4ee67c0fe7a04850
AbdullahAttique1 <possiblepossible51@gmail.com>
2026-02-23 15:09:16 +0500

    dsa pattern practice

---
+++ dsanumberpatetrn.cpp
@@ -0,0 +1,17 @@
+//dsaprintnumbertriangle
+#include<iostream>
+using namespace std;
+int main(){
+	int n=4;	
+
+	for(int i=0;i<n;i++){
+			int number=1;
+		for(int j=0;j<i+1;j++){
+			cout<<number;
+		number=number+1;
+		}
+			
+		cout<<endl;
+	}
+	
+}
