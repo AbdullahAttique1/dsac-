ac6a6227dbf376ab1a3bb8b7e725b1a66ddfeae5
AbdullahAttique1 <possiblepossible51@gmail.com>
2026-02-24 15:44:33 +0500

    allpatterns doe

--- patterns/butterflytrianglepattern.cpp
+++ patterns/butterflytrianglepattern.cpp
@@ -1,30 +1,35 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n=4;
+	int n=8;
 	for(int i=1;i<=n;i++){
+		int number=1;
 		for (int j=1; j<=i;j++){
-			cout<<"*";			
+			cout<<number;
+			number=number+1;			
 		}
 		for(int j=0;j<2*(n-i);j++){
 			cout<<" ";
 		}
-			for (int j=1; j<=i;j++){
-			cout<<"*";			
+			for (int j=i; j>0;j--){
+			cout<<j;
+					
 		}
 		cout<<endl;
 	}
 	
 	
-		for(int i=n;i>=0;i--){
+		for(int i=n-1;i>=0;i--){
+			int number=1;
 		for (int j=1; j<=i;j++){
-			cout<<"*";			
+			cout<<number;
+			number=number+1;		
 		}
 		for(int j=0;j<2*(n-i);j++){
 			cout<<" ";
 		}
-			for (int j=1; j<=i;j++){
-			cout<<"*";			
+			for (int j=i; j>0;j--){
+			cout<<j;			
 		}
 		cout<<endl;
 	}
