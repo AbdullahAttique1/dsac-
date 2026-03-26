06740e097904efa9b05233078c2f439bdceec7d4
AbdullahAttique1 <possiblepossible51@gmail.com>
2026-03-26 11:25:52 +0500

    move to strings and completed sortd

---
+++ sorting/next_permutation.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void findNextPermutaion(vector<int> &nums,int size){
+ 
+     int permitize=-1;
+    for(int i=size-2;i>=0;i--){
+        if(nums[i] < nums[i+1] ){
+            permitize=i;   
+            break;      
+        }
+    }
+
+    if(permitize == -1 ){
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+   for(int i=size-2;i>=0;i--){
+        if(  nums[i]   >  nums[permitize]  ){
+            swap(nums[i],  nums[permitize] );
+            break;      
+        }
+    }
+
+
+int i=permitize+1;
+int j=size-1;
+while(i<=j){
+    swap(nums[i], nums[j]);
+    i++;
+    j--;
+}
+    
+
+for(int val :nums){
+    cout<<val;
+}
+}
+int main() {
+        vector<int> nums = {1,2,3};   
+        int size=nums.size();
+findNextPermutaion(nums,size);
+    return 0;
+}
